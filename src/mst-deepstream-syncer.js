400955a7ead25554593e8bd3cfe3b2bdd9bb2dae
geohuz <geohuz@163.com>
2023-03-23 10:58:05 +0800

    a experimental cach getrecord function

--- src/mst-deepstream-syncer.js
+++ src/mst-deepstream-syncer.js
@@ -9,17 +9,24 @@ import isEqual from "lodash/isequal"
 // 2. reference: example done
 
 // 本地记录缓存
-const currentRecords = new Map()
 // 本地记录操作
-const getRecord = async(recordName) => {
-  let localRecord = currentRecords.get(recordName)
-  if (!record) {
-    remoteRecord = await dsc.record.getRecord(recordName)
-    remoteRecord.whenReady()
-    currentRecords.set(recordName, record.get())
-  }
-  return [localRecord, remoteRecord] 
+const currentRecords = new Map()
+const getRecord = async(recordName, applyData) => {
+  if (!currentRecords.get(recordName)) {
+    let recordHandler = dsc.record.getRecord(recordName)
+    await recordHandler.whenReady()
+    currentRecords.set(recordName, recordHandler.get())
+    recordHandler.subscribe(async(newData)=> {
+      if (!isEqual(currentRecords.get(recordName), newData)) {
+        currentRecords.set(recordName, newData)
+        applyData(newData)
+      }
+    })
+  } else {
+    applyData(currentRecords.get(recordName))
+  } 
 }
+
 const discardRecord = (recordName) => {
   const record = getRecord(recordName)
   record.discard()
@@ -44,28 +51,32 @@ export async function loadFromDS(node) {
     console.info("BackEnd list add sync to Frontend ", recordName)
     let record = dsc.record.getRecord(recordName)
     await record.whenReady()
-    record.subscribe(newData=> {
-      let [localRecord, _] = getRecord(recordName)
+    record.subscribe(async(newData)=> {
+      let localRecord = currentRecords.get(recordName) // 先查缓存
       if (!isEqual(localRecord, newData)) {
+        currentRecords.set(recordName, newData) // don't forget to update the cache!
         let idValue = getIdentifier(getChildType(node).create(newData))
         applySubscribedData(newData, node, idValue)
       }
     })
-    
-    unprotect(getRoot(node))
-    let recordContent = record.get()
-    node.put(recordContent)
-    // 它端list新加入记录的修改侦听
+    if (!currentRecords.get(recordName)) {
+      const rec = record.get()
+      currentRecords.set(recordName, rec)  // dont' forget to update the cache!
+      unprotect(getRoot(node))
+      node.put(rec)
+    }
   })
   // list 它端删除侦听
   list.on("entry-removed", async(recordName)=> {
-    console.info("BackEnd list remove sync to Frontend ", recordName)
-    let xs = recordName.split('/')
-    let recordId = xs[xs.length-1]
-    console.log("removed record id: ", recordId)
-    unprotect(getRoot(node))
-    destroy(node.get(recordId))
-    //remove(node, recordId)
+    if (currentRecords.get(recordName)) {
+      console.info("BackEnd list remove sync to Frontend ", recordName)
+      let xs = recordName.split('/')
+      let recordId = xs[xs.length-1]
+      console.log("removed record id: ", recordId)
+      unprotect(getRoot(node))
+      destroy(node.get(recordId))
+      currentRecords.delete(recordName)
+    }
   })
   console.info("list length: ", list.getEntries().length)
   // 首次同步
@@ -74,14 +85,19 @@ export async function loadFromDS(node) {
     await rec.whenReady()
     
     let recordContent = rec.get()
+    currentRecords.set(item, recordContent)
     // getChildType(node): User
     let idValue = getIdentifier(getChildType(node).create(recordContent))
     snapshot[idValue] = recordContent
 
     // 首次加入记录的它端修改侦听
     rec.subscribe(newData=> {
-      idValue = getIdentifier(getChildType(node).create(newData))
-      applySubscribedData(newData, node, idValue)
+      let localRecord = currentRecords.get(item) 
+      if (!isEqual(localRecord, newData)) {
+        currentRecords.set(item, newData)   // 更新缓存
+        idValue = getIdentifier(getChildType(node).create(newData))
+        applySubscribedData(newData, node, idValue)
+      }
     })
   }))
   console.info("before return snapshot: ", snapshot)
@@ -90,12 +106,12 @@ export async function loadFromDS(node) {
 
 function applySubscribedData(data, node, idValue) {
   console.log("entering applySubscribedData")
-  let recordNode = node.get(idValue)
-  let recordNodeValue = recordNode.toJSON()
-  if (!isEqual(data, recordNodeValue)) {
+  //let recordNode = node.get(idValue)
+  //let recordNodeValue = recordNode.toJSON()
+  //if (!isEqual(data, recordNodeValue)) {
     console.log("Backend record sync to Frontend: ", data)
     applySnapshot(recordNode, data)
-  }
+  //}
 }
 
 // 前端数据同步到后端数据
@@ -109,28 +125,26 @@ export async function triggerDSUpdate(treeNode, patch) {
   const pathXS = patch.path.split('/') 
   const listName = pathXS.slice(0,2).join('/')
   const recordName = pathXS.slice(0,3).join('/')
-  let localRecordContent = {}
-  let localRecordExist = await dsc.record.has(recordName)
-  if (localRecordExist) {
-    localRecordContent = dsc.record.getRecord(recordName).get()
-  } 
-  let localRecordIsEmpty = (Object.keys(localRecordContent).length==0)
+  let localRecordContent = currentRecords.get(recordName)
+  //let localRecordExist = await dsc.record.has(recordName)
+  //if (localRecordExist) {
+  //  localRecordContent = dsc.record.getRecord(recordName).get()
+  //} 
+  //let localRecordIsEmpty = (Object.keys(localRecordContent).length==0)
   console.info(`onPatch: `, patch)
-  console.info(`local record: recordName: ${recordName}  localRecordContent: ${JSON.stringify(localRecordContent)}`)
+  //console.info(`local record: recordName: ${recordName}  localRecordContent: ${JSON.stringify(localRecordContent)}`)
   switch (patch.op) {
     case "replace":
-      // todo: not euqal to local than replace
+      // todo: not euqal to local then replace
       let field = pathXS[pathXS.length-1]
-      if (localRecordContent[field] !== patch.value) {
+      //if (localRecordContent[field] !== patch.value) {
         console.info("Frontend replace sync to Backend: ", `recordName: ${recordName} field: ${field} value: ${JSON.stringify(patch.value)}`)
         dsc.record.setData(recordName, `${field}`, patch.value)
-      }
+      //}
       break
     case "add":
       // 本端内存record记录和patch.value不一样
-      if (!isEqual(patch.value, localRecordContent)) {
-        // 如果localRecordContent为空则向后端添加一条记录
-        if (localRecordIsEmpty) {
+      if (localRecordContent) {
           console.info("Frontend add sync to Backend: ", `recordName: ${recordName} value: ${JSON.stringify(patch.value)}`)
           let record = dsc.record.getRecord(recordName)
           await record.whenReady()
@@ -144,16 +158,18 @@ export async function triggerDSUpdate(treeNode, patch) {
           list.whenReady(()=>{
             list.addEntry(recordName)
           })
-        } else {
-          // 记录子属性add
-          // todo: 只处理了map(不过建议用map) 还需array处理
-          console.info("snapshotLeaf", snapshotLeaf)
-          console.info("localRecordContent", localRecordContent)
-          let newPatch = diff(localRecordContent, snapshotLeaf)
-          console.info("new patch: ", newPatch[0])
-          let [_, field, key] = newPatch[0].path.split('/')
-          console.info('field and key: ', field,  key)
-          dsc.record.setData(recordName, field, {[key]: patch.value})
+      } else {
+        if (!isEqual(patch.value, localRecordContent)) {
+          // 如果localRecordContent为空则向后端添加一条记录
+            // record子属性add
+            // todo: 只处理了map(不过建议用map) 还需array处理
+            console.info("snapshotLeaf", snapshotLeaf)
+            console.info("localRecordContent", localRecordContent)
+            let newPatch = diff(localRecordContent, snapshotLeaf)
+            console.info("new patch: ", newPatch[0])
+            let [_, field, key] = newPatch[0].path.split('/')
+            console.info('field and key: ', field,  key)
+            dsc.record.setData(recordName, field, {[key]: patch.value})
         }
       }
       break
