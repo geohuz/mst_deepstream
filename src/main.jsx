33893fc54b05d85dffc25cd1c2f48f8e3ddde4af
geohuz <geohuz@163.com>
2023-03-30 18:19:46 +0800

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,6 @@ import ReactDOM from 'react-dom/client'
 import Root from './Root'
 import './index.css'
 
-import Reactotron from 'reactotron-react-js'
-Reactotron.configure().connect()
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <DeepstreamClientProvider>
       <Root />
